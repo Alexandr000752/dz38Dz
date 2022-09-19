17ea386668e7d8d6ba9e9c783d396a6b4ff41fde
Alexandr000752 <112328813+Alexandr000752@users.noreply.github.com>
2022-09-19 20:13:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Write("Введите номер: ");
+int numberA = int.Parse(Console.ReadLine());
+
+int[] array = new int[numberA];
+
+for(int i = 0; i< array.Length; i++)
+{
+	array[i] = new Random().Next(1, 100);
+	Console.Write(array[i] + ", ");
+}
+
+PrintMaxMin(array);
+
+
+void PrintMaxMin(int[] array)
+{
+	int max = array[0];
+	int min = array[0];
+	
+	for(int index = 0; index < array.Length; index++)
+	{
+		if(max < array[index]) max = array[index];
+	
+     	if(min > array[index]) min =array[index];
+	}
+	Console.WriteLine("max: " + max + "; min: "+ min + ";");
+	Console.WriteLine("Разница между максимальным и минимальным числом будет ровна: " + (max-min));
+}
+
+
+
